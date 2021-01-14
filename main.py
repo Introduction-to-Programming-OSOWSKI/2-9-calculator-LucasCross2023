9678bf4f0a4472f7ba0b50c3a2f2f7f5cd126677
LucasCross2023 <817750@mystma.org>
2021-01-14 15:49:52 +0000

    calculator try 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def calculator(exprestion, x, y):
+    if exprestion == ("add"):
+        return x + y
+    elif exprestion ==  ("subtract"):
+        return x - y
+    elif exprestion == ("multiply"):
+        return x * y
+    elif exprestion == ("divide"):
+        return x / y
+
+print(calculator("multiply", 10, 3))
